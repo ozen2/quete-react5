0b126a01d36b95b7c6c10951d5062f4808744ce5
Enzo Michel <enzomichel399@gmail.com>
2024-03-27 17:21:08 +0100

    fnens

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,25 @@
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
 function PokemonCard() {
+    const pokemon = pokemonList[1];
     return (
         <>
             <figure>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
+                {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name} />
+                ) : (
+                    <p>???</p>
+                )}
             </figure>
-            <figcaption>Bulbasaur</figcaption>
+            <figcaption>{pokemon.name}</figcaption>
         </>
     )
 }
